440cf511b8a29b561803a53d53756ef2665be0c0
Caio Machado <caioamsul@gmail.com>
2021-03-12 15:06:05 -0300

    Created Activitys

---
+++ app/src/main/java/com/example/nailstime/minhaAgenda.kt
@@ -0,0 +1,11 @@
+package com.example.nailstime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class minhaAgenda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_minha_agenda)
+    }
+}
