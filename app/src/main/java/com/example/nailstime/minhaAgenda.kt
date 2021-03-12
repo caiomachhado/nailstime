2c92c6c6b9e7a0e10398b8fe9376660a950f81e5
Caio Machado <caioamsul@gmail.com>
2021-03-12 15:11:23 -0300

    Button from Agenda

--- app/src/main/java/com/example/nailstime/minhaAgenda.kt
+++ app/src/main/java/com/example/nailstime/minhaAgenda.kt
@@ -1,11 +1,22 @@
 package com.example.nailstime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class minhaAgenda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_minha_agenda)
+
+        var buttonAgenda : AppCompatButton = findViewById(R.id.verAgenda)
+        var buttonHorario : AppCompatButton = findViewById(R.id.marcarHorario)
+
+        buttonAgenda.setOnClickListener {
+            Intent intent = new Intent(this, minhaAgenda)
+            startActivity(intent)
+        }
+
     }
 }
